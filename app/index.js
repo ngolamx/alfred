32763292c513ace2bbfbae340b3fce4f9fe2a6d3
Ngo Lam <lam@ngolam.xyz>
2021-04-27 18:06:51 +0700

    add dialog

--- app/index.js
+++ app/index.js
@@ -1,6 +1,12 @@
 (function() {
   //Dependencies
-  $script(['app/vendor/ready.js', 'app/vendor/reqwest.min.js', 'app/vendor/lazyload.js'], 'bundle');
+  var lib = [
+  'app/vendor/ready.js',
+  'app/vendor/reqwest.min.js',
+  'app/vendor/lazyload.js',
+  'app/vendor/picoModal-3.0.0.min.js',
+  ];
+  $script(lib, 'bundle');
 
   var logoutHandler = function() {
     document.querySelector('.logout').addEventListener('click', function(e) {
@@ -12,10 +18,38 @@
     });
   }
 
+  var addOrderHandler = function() {
+    var plusEle = document.querySelector('#add-order');
+    var dialog;
+    if (plusEle) {
+      plusEle.addEventListener('click', function(e) {
+        e.preventDefault();
+        dialog.show();
+      });
+      reqwest('/app/templates/add-order.html').then(function(resp) {
+         dialog = picoModal({
+           content: resp,
+           overlayClose: false,
+           modalClass: 'modal',
+           closeClass: 'modal-close',
+           closeHtml: `
+              <svg class="icon-close">
+                <use xlink:href="app/img/sprite.svg#icon-close"></use>
+              </svg>
+           `
+         }).afterCreate(function(modal){
+            modal.modalElem().getElementsByClassName("dismiss")[0]
+                .addEventListener('click', modal.close);
+        });
+      });
+    }
+  }
+
   var app = function() {
     reqwest('app/templates/index.html', function (resp) {
       document.querySelector('#app').insertAdjacentHTML('afterbegin', resp);
       logoutHandler();
+      addOrderHandler();
     });
     LazyLoad.css('app/css/style.css');
 
